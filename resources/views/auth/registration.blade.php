448e7caa23d2b5d1cd08692a2826d6b87cc9b856
Valentino Eka Apritha Putra <fiyunecontact@gmail.com>
2024-05-16 14:39:03 +0700

    add simple view

---
+++ resources/views/auth/registration.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ asset('assets/css/bootstrap.min.css') }} " rel="stylesheet">
+    <title>Yezc | Auth</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class=""></div>
+        </div>
+    </div>
+</body>
+</html>

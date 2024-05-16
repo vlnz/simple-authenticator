b9ff56066953e99bfd7e231119f5280a922cdd25
Valentino Eka Apritha Putra <fiyunecontact@gmail.com>
2024-05-16 14:58:01 +0700

    beta web auth

--- resources/views/auth/registration.blade.php
+++ resources/views/auth/registration.blade.php
@@ -10,8 +10,60 @@
 <body>
     <div class="container">
         <div class="row">
-            <div class=""></div>
+            <div class="col-md-col-md-offset-4">
+                <h2>Registration</h2>
+                <form action="{{route('register-user')}}" method="post" enctype="multipart/form-data">
+                    @csrf
+                    @if (Session::has('success'))
+                        <div class="alert alert-success">
+                            {{Session::get('success')}}
+                        </div>
+                    @endif
+                    @if (Session::has('fail'))
+                    <div class="alert alert-danger">
+                        {{Session::get('fail')}}
+                    </div>
+                @endif
+                    <div class="form-group">
+                        <label for="name">Full Name</label>
+                        <input type="text" name="name" class="form-control">
+                        <span class="text-danger">
+                            @error('name')
+                                {{$message}}
+                            @enderror
+                        </span>
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" name="email" class="form-control">
+                        <span class="text-danger">
+                            @error('email')
+                                {{$message}}
+                            @enderror
+                        </span>
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control">
+                        <span class="text-danger">
+                            @error('password')
+                                {{$message}}
+                            @enderror
+                        </span>
+                    </div>
+
+                    <br>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-block btn-success">Registration</button>
+                    </div>
+                    <br>
+                    <a href="login">Login Here!</a>
+                </form>
+            </div>
         </div>
+
+        <script src="assets/js/boostrap.min.js"></script>
     </div>
-</body>
+
+  </body>
 </html>
